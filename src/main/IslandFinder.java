4a9ef21a2287daa93a657a17bc0a6277b7740f21
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-04-14 12:37:44 -0400

    - General code debugging

--- src/main/IslandFinder.java
+++ src/main/IslandFinder.java
@@ -7,19 +7,27 @@ public class IslandFinder {
 							   { 0, 1, 1, 0, 0 }, 
 							   { 0, 0, 1, 0, 1 }, 
 							   { 1, 0, 0, 0, 1 },
-							   { 0, 1, 0, 1, 1 },
-							   { 1, 0, 0, 0, 0 } };
+							   { 1, 1, 0, 0, 1 },
+							   { 1, 1, 1, 0, 0 } };
 	static final int ROWS = 6;
 	static final int COLS = 5;
+	
 	static int size = 0;
 	static int tempSize = 0;
-	static int[][] tempIsland = new int[ROWS][COLS];
+	
 	static int[][] biggestIsland = new int[ROWS][COLS];
+	static int[][] tempIsland = new int[ROWS][COLS];
+	
 	static int[][] discovered = new int[ROWS][COLS];
+	
 	public static void main(String[] args) {
 
-		findBiggestIsland(0, 0);
-
+		findBiggestIsland();
+		
+		System.out.println("The biggest island is of size " + size + "\n");
+		System.out.println("Isolated on the map it appears as below:");
+		
+		
 		for (int[] row : biggestIsland) {
 			for (int x : row) {
 				System.out.print(x + " ");
@@ -30,16 +38,21 @@ public class IslandFinder {
 	
 	
 	
-	private static void findBiggestIsland(int row, int col){
+	private static void findBiggestIsland(){
 		
 		for(int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLS; j++) {
-				if(promising(row,col)){
-					tempSize += 1;
-					discovered[row][col] = 1;
-					tempIsland[row][col] = 1;
-					checkAround(row,col);
+				if(promising(i,j)){
+					checkAround(i,j);
+					if (tempSize > size) {
+						biggestIsland = tempIsland;
+						size = tempSize;
+					}
+					tempSize = 0;
+					tempIsland = new int[ROWS][COLS];
+					
 				}
+				
 			}
 		
 		}
@@ -52,39 +65,42 @@ public class IslandFinder {
 		}
 		// Check right-above diagonal
 		if(promising(row-1,col+1)) {
-			checkAround(row-1,col);
+			checkAround(row-1,col+1);
 		}
 		// Check right
 		if(promising(row,col+1)) {
-			checkAround(row-1,col);
+			checkAround(row,col+1);
 		}
 		// Check right-below diagonal
 		if(promising(row+1,col+1)) {
-			checkAround(row-1,col);
+			checkAround(row+1,col+1);
 		}
 		// Check below
 		if(promising(row+1,col)) {
-			checkAround(row-1,col);
+			checkAround(row+1,col);
 		}
 		// Check left-below diagonal
 		if(promising(row+1,col-1)) {
-			checkAround(row-1,col);
+			checkAround(row+1,col-1);
 		}
 		// Check left
 		if(promising(row,col-1)) {
-			checkAround(row-1,col);
+			checkAround(row,col-1);
 		}
 		// Check left-above diagonal
 		if(promising(row-1,col-1)) {
-			checkAround(row-1,col);
+			checkAround(row-1,col-1);
 		}
 	}
 		
 	private static boolean promising(int row, int col) {
 
+		if(row < 0 || col < 0 || row >= ROWS || col >= COLS)
+			return false;
+			
 		if(islands[row][col] == 1 && discovered[row][col] == 0){
 			tempSize += 1;
-			discovered[row-1][col] = 1;
+			discovered[row][col] = 1;
 			tempIsland[row][col] = 1;
 			return true;
 		}
