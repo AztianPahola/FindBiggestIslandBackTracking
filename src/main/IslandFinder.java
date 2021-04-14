3072d8392b609cca5f977cf0030397df7bb43d08
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-04-13 22:41:07 -0400

    - Plan to separate recursive part of the code from the iteration part, as this was difficult to have in one. Headers and body have been modified as such.

--- src/main/IslandFinder.java
+++ src/main/IslandFinder.java
@@ -5,9 +5,13 @@ public class IslandFinder {
 	public static void main(String[] args) {
 
 		// Test array
-		int[][] islands = { { 1, 1, 0, 0, 0 }, { 0, 1, 1, 0, 0 }, { 0, 0, 1, 0, 1 }, { 1, 0, 0, 0, 1 },
-				{ 0, 1, 0, 1, 1 }, { 1, 0, 0, 0, 0 } };
-		int[][] biggestIsland = findBiggestIsland(islands);
+		int[][] islands = { { 1, 1, 0, 0, 0 }, 
+							{ 0, 1, 1, 0, 0 }, 
+							{ 0, 0, 1, 0, 1 }, 
+							{ 1, 0, 0, 0, 1 },
+							{ 0, 1, 0, 1, 1 },
+							{ 1, 0, 0, 0, 0 } };
+		int[][] biggestIsland = findBiggestIsland(islands, 0, 0);
 
 		for (int[] row : biggestIsland) {
 			for (int x : row) {
@@ -16,28 +20,85 @@ public class IslandFinder {
 			System.out.println();
 		}
 	}
-
-	private static int[][] findBiggestIsland(int[][] islands){
+	
+	static final int ROWS = 6;
+	static final int COLS = 5;
+	static int size = 0;
+	static int tempSize = 0;
+	static int[][] tempIsland = new int[ROWS][COLS];
+	static int[][] biggestIsland = new int[ROWS][COLS];
+	static int[][] discovered = new int[ROWS][COLS];
+	
+	private static void findBiggestIsland(int[][] islands, int row, int col, int size){
 		
-		int rows = islands.length;
-		int cols = islands[0].length;
-		
-		int[][] discovered = new int[rows][cols];
-		int[][] biggestIsland = new int[rows][cols];
-		
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if(islands[i][j] == 1) {
-					while (promising(i,j)){
-						discovered[i][j] = 1;
-					}
+		for(int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				if(promising(row,col)){
+					tempSize += 1;
+					discovered[row][col] = 1;
+					tempIsland[row][col] = 1;
+					checkAround(row,col);
 				}
 			}
+		
 		}
 	}
 	
-	private static boolean promising(int row, int col) {
+	private static void checkAround(int row, int col) {
 		
+	}
+		
+		
+//		int[][] tempIsland = new int[ROWS][COLS];
+//		int size = 0;
+//		
+//		
+//		if (row == ROWS-1) {
+//			if (col == COLS-1) {
+//				return;
+//			}
+//			else {
+//				if (promising(row,col)){
+//					size++;
+//					tempIsland[row][col] = 1;
+//					discovered[row][col] = 1;
+//					size++;
+//				}
+//				else {
+//					if(size > )
+//					size = 0;
+//					
+//				}
+//				findBiggestIsland(islands,row, col+1,size);
+//			}
+//		}
+//		else {
+//			if (col == COLS-1) {
+//				if (promising(row,col)){
+//					size++;
+//					tempIsland[row][col] = 1;
+//					discovered[row][col] = 1;
+//					findBiggestIsland(islands,);
+//				}
+//			}
+//			else {
+//				if (promising(row,col)){
+//					size++;
+//					tempIsland[row][col] = 1;
+//					discovered[row][col] = 1;
+//					findBiggestIsland(islands,);
+//				}
+//			}
+//		}
+//				if (promising(row,col)){
+//					size++;
+//					tempIsland[row][col] = 1;
+//					discovered[row][col] = 1;
+//				}
+//				findBiggestIsland(islands,i);
+
+	private static boolean promising(int row, int col) {
+
 	}
 
 }
